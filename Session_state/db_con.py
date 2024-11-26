556c4d1579835e9067a976245140526007a21b57
Sourav Garodia <96367006+garodiaa@users.noreply.github.com>
2024-11-26 21:46:18 +0600

    jj

--- Session_state/db_con.py
+++ Session_state/db_con.py
@@ -1,44 +1,25 @@
 import streamlit as st
-import pandas as pd
 
 import mysql.connector
 
-# Function to create a connection to the MySQL database
-def create_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root@localhost",
-        password="",
-        database="shelfie"
-    )
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="noticedb"
+)
+cursor = connection.cursor()
 
-# Function to fetch data from a table
-def fetch_data(connection, query):
-    cursor = connection.cursor()
-    cursor.execute(query)
-    data = cursor.fetchall()
-    columns = [desc[0] for desc in cursor.description]
-    return pd.DataFrame(data, columns=columns)
+print("Connected to the database")
 
 # Streamlit app
 def main():
     st.title("MySQL Database Viewer")
 
-    # Connect to the database
-    connection = create_connection()
-    print("Connected to the database")
-
-    # Query to fetch data from a table
-    # query = "SELECT * FROM your_table"
-
-    # # Fetch data
-    # data = fetch_data(connection, query)
-
-    # # Display data in Streamlit
-    # st.write(data)
-
-    # # Close the connection
-    # connection.close()
+    st.subheader("View Data")
+    cursor.execute("SELECT * FROM students")
+    data = cursor.fetchall()
+    st.write(data)
 
 if __name__ == "__main__":
     main()
